9637c690e61d84fb75e4ac74de32242029550425
Hyungrok Kim <hyungrokkim@gmail.com>
2024-07-30 14:49:31 +0100

    Add template to make the TOML the single source of truth

--- convert.py
+++ convert.py
@@ -1,14 +1,22 @@
 import tomllib
-print("""BEGIN:VCALENDAR
-VERSION:2.0""")
 
+with open("calendar.ics", "w") as c: 
+    print("BEGIN:VCALENDAR\nVERSION:2.0", file=c)
+    with open("data.toml", "rb") as f:
+        for entry in tomllib.load(f).values():
+            print("BEGIN:VEVENT", file=c)
+            date = entry['date'].strftime("%Y%m%d")
+            print(f"DTSTART;VALUE=DATE:{date}T150000", file=c)
+            print(f"DTEND;VALUE=DATE:{date}T160000", file=c)
+            print(f"SUMMARY:{entry['name']} ({entry['institution']}), {entry.get('title','TBA')}", file=c)
+            print(f"DESCRIPTION:{entry.get('abstract','TBA')}", file=c)
+            print("END:VEVENT", file=c)
+    print("END:VCALENDAR", file=c)
+
+s = ""
 with open("data.toml", "rb") as f:
-    for entry in tomllib.load(f).values():
-        print("BEGIN:VEVENT")
-        date = entry['date'].strftime("%Y%m%d")
-        print(f"DTSTART;VALUE=DATE:{date}T150000")
-        print(f"DTEND;VALUE=DATE:{date}T160000")
-        print(f"SUMMARY:{entry['name']} ({entry['institution']}), {entry.get('title','TBA')}")
-        print(f"DESCRIPTION:{entry.get('abstract','TBA')}")
-        print("END:VEVENT")
-print("END:VCALENDAR")
+    for entry in sorted(tomllib.load(f).values(), key=lambda e: e['date']):
+        s += f"<details><summary>{entry['date']}: {entry['name']} ({entry['institution']}), {entry.get('title','TBA')}</summary>{entry.get('abstract','TBA')}</details>"
+
+with open("index.html", "w") as f:
+    print(open("template.html", "r").read().replace("####", s), file=f)
