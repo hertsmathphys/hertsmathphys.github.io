1853f2923cef1b63ecb4f5fd866fc823aa7006db
Julian Kupka <J.Kupka@herts.ac.uk>
2025-09-15 23:03:12 +0100

    Move entry creation to separate function

--- convert.py
+++ convert.py
@@ -3,7 +3,21 @@ import datetime
 import zoneinfo
 import re
 
+
+def make_current_entry(date, location, name, institution, title, abstract):
+    time = f"<time datetime='{date}'>{date.strftime("%-d %B, %H:%M")}</time>"
+    summary = f"{time}, {location}: {name} ({institution}), {title}"
+    return (f"<details><summary>{summary}</summary>{abstract}</details>")
+
+
+def make_past_entry(date, name, institution, title, abstract):
+    time = f"<time datetime='{date}'>{date.strftime("%-d %B %Y, %H:%M")}</time>"
+    summary = f"{time}: {name} ({institution}), {title}"
+    return (f"<details><summary>{summary}</summary>{abstract}</details>")
+
+
 strip = lambda s: re.sub("<[^>]*>", "", s)
+
 strip.__doc__ = "Remove HTML tags from string"
 
 localise = lambda d: datetime.datetime.combine(d.date(), d.time(), zoneinfo.ZoneInfo('Europe/London'))
@@ -34,12 +48,25 @@ current_academic_year_start = datetime.datetime((n:=datetime.datetime.now()).yea
 current_year_entries = ""
 with open("data.toml", "rb") as f:
     for entry in filter( lambda e: e['date'] >= current_academic_year_start, sorted(tomllib.load(f).values(), key=lambda e: e['date'], reverse=False)):
-        current_year_entries += f"<details><summary><time datetime='{entry['date']}'>{entry['date'].strftime("%-d %B, %H:%M")}</time>, {entry['location']}: {entry['name']} ({entry['institution']}), {entry.get('title','TBA')}</summary>{entry.get('abstract','TBA')}</details>"
+        current_year_entries += make_current_entry(
+            entry['date'],
+            entry['location'],
+            entry['name'],
+            entry['institution'],
+            entry.get('title', 'TBA'),
+            entry.get('abstract', 'TBA')
+        )
 
 past_year_entries = ""
 with open("data.toml", "rb") as f:
     for entry in filter(lambda e: e['date'] < current_academic_year_start, sorted(tomllib.load(f).values(), key=lambda e: e['date'], reverse=True)):
-        past_year_entries += f"<details><summary><time datetime='{entry['date']}'>{entry['date'].strftime("%-d %B %Y, %H:%M")}</time>: {entry['name']} ({entry['institution']}), {entry.get('title','TBA')}</summary>{entry.get('abstract','TBA')}</details>"
+        past_year_entries += make_past_entry(
+            entry['date'],
+            entry['name'],
+            entry['institution'],
+            entry.get('title', 'TBA'),
+            entry.get('abstract', 'TBA')
+        )
 
 with open("index.html", "w") as f:
     print(open("template.html", "r").read().replace("$current_year", current_year_entries).replace("$past_years", past_year_entries), file=f)
